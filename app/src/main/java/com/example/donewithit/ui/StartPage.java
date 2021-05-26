7a28d77f3f980ac043cbe6481df362c201220911
AbdulmalekAlshugaa <alshugaa19@gmail.com>
2021-05-26 13:31:43 +0800

    start page

---
+++ app/src/main/java/com/example/donewithit/ui/StartPage.java
@@ -0,0 +1,16 @@
+package com.example.donewithit.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.donewithit.R;
+
+public class StartPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_page);
+    }
+}
